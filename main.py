f15db6fd56cfda60a3f0992120a8d48e14219aa4
Nic0ro <willarossi1@gmail.com>
2025-12-15 14:21:58 -0500

    Add lin reg and knn

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import scrubber 
+import linear_regression
+from k_neighbor import k_nei
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 df=pd.read_csv('bank-full.csv',sep=';')
-
-
+print(df.head())
 df=scrubber.scrubber.clean(df)
+print(df.head())
+
+x_vals, y_line = linear_regression.lin_reg_calc.calc(df)
+
+plt.plot(x_vals,y_line)
+plt.show()
--- scrubber.py
+++ scrubber.py
@@ -4,12 +4,12 @@ class scrub:
 
     drop=['contact','day','month','duration','poutcome']
     job={'housemaid':0,'services':1,'admin.':2,'blue-collar':3,'technician':4,'retired':5,'management':6,'unemployed':7,'self-employed':8,'unknown':9,'entrepreneur':10,'student':11}
-    marital={'married':0,'single':1,'divorced':2,'unkown':3}
-    education={'unkown':0,'primary':1,"secondary":2,'tertiary':3}
-    default={'no':0,'unknown':1,'yes':2}
-    housing={'no':0,'yes':1,'unknown':2}
-    loan={'no':0,'yes':1,'unknown':2}
-    y={'no':0,'yes':1,'unkown':2}
+    marital={'married':3,'single':1,'divorced':2,'NaN':0,"unknown":0}
+    education={'NaN':0,'primary':1,"secondary":2,'tertiary':3,'unknown':0}
+    default={'no':1,'unknown':1,'yes':0}
+    housing={'no':0,'yes':1,'unknown':0}
+    loan={'no':0,'yes':1,'unknown':0}
+    y={'no':0,'yes':1,'NaN':0,'unknown':0}
     
 
 
@@ -24,8 +24,9 @@ class scrub:
         for i in df.columns:
             if i in maps:
                 df[i]=df[i].map(maps[i])
-
-        print(df.head(5))
+        print(df.columns)
+        #df = pd.get_dummies(df, columns=['default'], drop_first=True)
+        df=df.dropna()
         return df
 
 scrubber=scrub()
